9c2fc1fc29a3fd4a4318dd7f827b8e9709a6aa0d
henricobzt <henricope792@gmail.com>
2025-03-31 15:18:20 -0300

    refactor: ajustar estrutura de pastas e tratamento de diretórios

--- data_transform/main.py
+++ data_transform/main.py
@@ -6,16 +6,22 @@ from pathlib import Path
 
 if __name__ == "__main__":
     BASE_DIR = Path(__file__).parent
-    PDF_PATH = Path(BASE_DIR/"pdf_files/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
-    CSV_PATH = Path(BASE_DIR/"csv_files/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.csv")
-    ZIP_PATH = Path(BASE_DIR/"zip_files")
+    CSV_DIR = Path(BASE_DIR/"csv_files")
+    PDF_DIR = Path(BASE_DIR/"pdf/")
+    ZIP_DIR= Path(BASE_DIR/"zip_files")
+    
+
+    CSV_PATH = Path(CSV_DIR/"Anexo_I_Rol_2021RN_465.2021_RN627L.2024.csv")
+    PDF_PATH = Path(PDF_DIR/"Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
+    ZIP_NAME = "Teste_Henrico.zip"
     EXPECTED_COLUMNS = 13
+    
     df = extract_tables_pdf(PDF_PATH, EXPECTED_COLUMNS)
     
     if df is not None and not df.empty:
         df = rename_columns(df)
         save_csv(df, CSV_PATH)
-        csv_compact_zip(CSV_PATH,ZIP_PATH,"Teste_Henrico.zip")
+        csv_compact_zip(CSV_PATH,ZIP_DIR, ZIP_NAME)
         
     else:
         print("⚠️ Nenhum dado foi processado. Verifique o PDF de entrada.")
