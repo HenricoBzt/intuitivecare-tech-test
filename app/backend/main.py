5d9d25e402a20e42ccde7629eb0ba05741fd5b70
Henrico Bazante <66339077+HenricoBzt@users.noreply.github.com>
2025-04-04 11:41:12 -0300

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -2,12 +2,7 @@ from app.backend.database import Base, engine
 from app.backend.router import operadoras
 from app.backend.population import population_operadoras
 from contextlib import asynccontextmanager
-"""
-Import the CORSMiddleware from FastAPI to handle Cross-Origin Resource Sharing (CORS) configurations.
 
-This middleware allows configuring cross-origin requests, enabling web applications 
-to make requests to the API from different domains by setting appropriate HTTP headers.
-"""
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
